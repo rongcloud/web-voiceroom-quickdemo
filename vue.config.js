6822024feb9f60835e3b973b03881cf847185ad1
cuifengbo <cuifengbo@cuifengbodeMacBook-Pro.local>
2021-10-19 18:05:53 +0800

    urifix

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,9 @@
+const  path  =  require('path')
 module.exports = {
+    publicPath:'/webchat/',
     devServer: {
         port: 8078,     // 端口号
         disableHostCheck: true
     },
-    assetsPublicPath: '/webchat/',
+  
 };
