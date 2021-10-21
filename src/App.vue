d808096663fb696076597af62f189601ab071fdb
cuifengbo <cuifengbo@cuifengbodeMacBook-Pro.local>
2021-10-21 16:00:49 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -234,8 +234,8 @@
 
 <script>
 //import sdk from "/Users/cuifengbo/work/RCVoiceRoomLib-Web/dist/main.js";
-// import sdk from "rcvoiceroomlib-v1"; 
-import sdk from "../../../work/RCVoiceRoomLib-Web/dist/main.js"; 
+ import sdk from "rcvoiceroomlib-v1"; 
+//import sdk from "../../../work/RCVoiceRoomLib-Web/dist/main.js"; 
 export default {
   name: "App",
   data: () => {
